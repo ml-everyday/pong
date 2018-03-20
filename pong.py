c8b1a578c80d9b8dece5118d9bcf9b451f750fb1
MLeveryday <machinelearningeveryday@gmail.com>
2018-03-20 19:37:51 +0100

    add initial structure

---
+++ pong.py
@@ -0,0 +1,34 @@
+import pygame
+import numpy as np
+
+def main():
+	pong = Pong()
+
+class Pong:
+	"""
+	Initialize and run a game of pong with two active players
+	"""
+	def __init__(self):
+		pass
+
+	def initialize_game(self):
+		pass
+
+	def spawn_ball(self, to_left):
+		pass
+
+	def update_game_position(self):
+		pass
+
+	def draw_screen(self):
+		pass
+
+	def keydown(self, event):
+		pass
+
+	def keyup(self, event):
+		pass
+
+
+if __name__ == "__main__":
+	main()
